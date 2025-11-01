23d191666fe9946f1657fdefe56e915b38c80377
powiedl <rolandpi@gmx.at>
2025-11-01 19:58:31 +0100

    104-Analytics Routes - getProfile controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -133,6 +133,9 @@ export const refreshToken = async (req, res) => {
 
 export const getProfile = async (req, res) => {
   try {
-    res.json({ message: 'endpoint is not implemented yet' });
-  } catch (error) {}
+    res.json(req.user);
+  } catch (error) {
+    console.log('Error in getProfile controller', error.message);
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
 };
